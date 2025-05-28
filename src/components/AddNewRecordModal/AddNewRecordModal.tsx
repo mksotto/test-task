e93a464e6516667f84b6964439530f0ebe173275
mksotto <maks.otto06@mail.ru>
2025-05-28 17:48:12 +0300

    messages after api post req added into useHumanPost hook

--- src/components/AddNewRecordModal/AddNewRecordModal.tsx
+++ src/components/AddNewRecordModal/AddNewRecordModal.tsx
@@ -1,21 +1,97 @@
 import {FC} from "react";
-import {Input, Modal, Form, Button} from "antd";
+import {Input, Modal, Form, FormProps, InputNumber} from "antd";
+import {useHumanPost} from "@/hooks/useHumanPost.ts";
+import {Human} from "@/types/api.ts";
+import {v7 as uuid} from "uuid";
 
 type Props = {
     isOpen: boolean;
-    setIsOpen: (isOpen: boolean) => void;
-}
+    onClose: () => void;
+};
 
-export const AddNewRecordModal: FC<Props> = ({isOpen, setIsOpen}) => {
+type HumanFormType = Omit<Human, 'id'>;
 
-
+export const AddNewRecordModal: FC<Props> = ({isOpen, onClose}) => {
+    const [form] = Form.useForm();
+    const {mutate} = useHumanPost();
+    const onFinish: FormProps<HumanFormType>['onFinish'] = (v) => {
+        const id = uuid();
+        mutate({id, ...v });
+        onClose();
+    };
     return (
-        <Modal open={isOpen} onOk={() => setIsOpen(false)}>
-            <Form>
-                <Form.Item>
-                    <Input type="text"/>
+        <Modal
+            title='Добавить запись'
+            open={isOpen}
+            onOk={form.submit}
+            onCancel={onClose}
+            destroyOnHidden
+        >
+            <Form
+                form={form}
+                requiredMark={false}
+                labelAlign='left'
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 20 }}
+                onFinish={onFinish}
+                preserve={false}
+            >
+                <Form.Item<HumanFormType>
+                    name='firstName'
+                    label='Имя'
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите имя'},
+                    ]}
+                >
+                    <Input/>
                 </Form.Item>
-                <Button type='default'>закончить</Button>
+                <Form.Item<HumanFormType>
+                    name='lastName'
+                    label='Фамилия'
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите фамилию'},
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+                <Form.Item<HumanFormType>
+                    name='age'
+                    label='Возраст'
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите возраст'}
+                    ]}
+                >
+                    <InputNumber controls={false} max={100} style={{ width: '100%' }}/>
+                </Form.Item>
+                <Form.Item<HumanFormType>
+                    name='city'
+                    label='Город'
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите город'},
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+                <Form.Item<HumanFormType>
+                    name='country'
+                    label='Страна'
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите страну'},
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+                <Form.Item<HumanFormType>
+                    name='email'
+                    label='Эл. почта'
+                    validateDebounce={1000}
+                    rules={[
+                        {required: true, message: 'Пожалуйста, введите электронную почту'},
+                        {type: 'email', message: 'Невалидный адрес электронной почты'}
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
             </Form>
         </Modal>
     );
--- src/hooks/useHumanPost.ts
+++ src/hooks/useHumanPost.ts
@@ -2,15 +2,18 @@ import {InfiniteData, useMutation, useQueryClient} from "@tanstack/react-query";
 import {Human} from "@/types/api.ts";
 import {humansPost} from "@/api/humans/humansPost.ts";
 import {PageResponse} from "@/types/PageResponse.ts";
+import {App} from "antd";
 
 export const useHumanPost = () => {
     const queryClient = useQueryClient();
+    const {message} = App.useApp();
     return useMutation<Human, Error, Human>({
         mutationFn: humansPost,
         onSuccess: (data) => queryClient.setQueryData<InfiniteData<PageResponse<Human>, number>>(
             ['humans'],
             (oldData) => {
                 if (!oldData || !oldData.pages[0]) return oldData;
+                void message.success('Запись успешно добавлена');
                 return {
                     ...oldData,
                     pages: [
@@ -22,6 +25,9 @@ export const useHumanPost = () => {
                     ],
                 };
             }),
-        onError: (error) => console.error('Ошибка создания:', error),
+        onError: (error) => {
+            void message.error('Произошла ошибка');
+            console.error('Ошибка создания:', error)
+        },
     });
 };
